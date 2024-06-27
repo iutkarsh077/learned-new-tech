462da2731ce1c8081a086b5ecba5b54879237b28
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-06-27 16:41:33 +0530

    synamic rendering

--- src/helpers/Db.ts
+++ src/helpers/Db.ts
@@ -1,8 +1,13 @@
+export const dynamic = "force-dynamic";
 import prisma from "../../prisma";
+import { cookies } from 'next/headers'
 
 const dbConnect = async () => {
     try {
+
         await prisma.$connect();
+        const cookieStore = cookies()
+        const theme = cookieStore.get('theme');
         console.log("Database connected");
     } catch (error) {
         console.error("Database connection failed", error);
